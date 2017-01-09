7f512d44e30e73bf09865fe9a13eb542b73af90c
Casey Kelso <casey@caseykelso.com>
2017-01-09 22:58:25 +0800

    command line parsing

--- app/main.cpp
+++ app/main.cpp
@@ -5,6 +5,67 @@
 #include "serialport/libserialport.h"
 #include "docopt/docopt.h"
 #include <iostream>
+#include <string>
+
+
+void transmit(std::string port, std::string baud)
+{
+   std::cout << "transmit: " << port << "," << baud << std::endl;
+}
+
+void receive(std::string port, std::string baud)
+{
+   std::cout << "receive: " << port << "," << baud << std::endl;
+}
+
+bool get_port(std::map<std::string, docopt::value> args, std::string &port)
+{
+  bool result = false;
+  auto p = args.find("--port")->second;
+
+  if (p.isString())
+  {
+     port   = p.asString();
+     result = true;
+  }
+
+  return result;
+}
+
+bool get_baud(std::map<std::string, docopt::value> args, std::string &baud)
+{
+  bool result = false;
+  auto b = args.find("--baud");
+
+  if (b != args.end())
+  {
+	  if (b->second.isString())
+	  {
+	     baud   = b->second.asString();
+	     result = true;
+	  }
+   }
+
+  return result;
+}
+
+std::string get_baud(std::map<std::string, docopt::value> args)
+{
+  std::string result = "";
+  auto port = args.find("--port")->second;
+
+  if (port.isString())
+  {
+    result = port.asString();
+  }
+  else
+  {
+     throw std::exception();
+  }
+
+  return result;
+}
+
 
 int main (int argc, char **argv )
 {
@@ -13,32 +74,56 @@ static const char USAGE[] =
 R"(nanoXmodem.
 
     Usage:
-      x --port=<port node>
-      x --baud=<baud rate>
-      x --receive
-      x --transmit --file=foo
+      x --port=<port> --receive [--baud=<baudrate>]
+      x --port=<port> --transmit --file=<filename> [--baud=<baudrate>]
       x (-h | --help)
       x --version
 
     Options:
       -h --help           Show this screen.
       --version           Show version.
-      --port=<comport>    Path to comport
-      --baud=<baudrate>   9600,19200,38400,57600,115200
-      --receive           Start transfer as receiver
-      --transmit          Start file transmission
-      --file=<filename>   File to send
+      --port=<comport>    Path to comport.
+      --baud=<baudrate>   Baudrate [default: 115200].
+      --receive           Start transfer as receiver.
+      --transmit          Start file transmission.
+      --file=<filename>   File to send.
 )";
 
    std::map<std::string, docopt::value> args
         = docopt::docopt(USAGE,
                          { argv + 1, argv + argc },
                          true,                // show help if requested
-                         "nanoeXmodem 0.1");  // version string
+                         "nanoXmodem 0.1");  // version string
 
+  if(args.find("--transmit") != args.end())
+  {
+    std::string port = "";
+    std::string baud = "";
+
+    get_port(args, port);
+    get_baud(args, baud);
+    transmit(port, baud);
+
+  }
+    else if (args.find("--receive") != args.end())
+  {
+    std::string port = "";
+    std::string baud = "";
+
+    get_port(args, port);
+    get_baud(args, baud);
+    receive(port, baud);
+
+  } 
+
+#if 0
    for(auto const& arg : args) 
    {
         std::cout << arg.first <<  arg.second << std::endl;
    }   
+#endif
+
   return 0;
 }
+
+
