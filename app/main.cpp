6f33a4ef545cdd82b65b8839f64cd3e66955d22c
Casey Kelso <casey@caseykelso.com>
2017-01-09 21:56:26 +0800

    xmodem app builds

--- app/main.cpp
+++ app/main.cpp
@@ -3,9 +3,11 @@
 #include "xmodem/xmodem_receiver.h"
 #include "xmodem/xmodem_transmitter.h"
 #include "serialport/libserialport.h"
+#include "docopt/docopt.h"
+#include <iostream>
 
 int main (int arc, char **argv )
 {
-   
-   return 0;
+     
+  return 0;
 }
