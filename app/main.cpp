42605fe4f9a3b3662d8f68cd04f1aa0770b9af17
Casey Kelso <casey@caseykelso.com>
2017-01-09 21:27:20 +0800

    setup build for xmodem cmd line app, add docopt as dep

---
+++ app/main.cpp
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include "xmodem/xmodem.h"
+#include "xmodem/xmodem_receiver.h"
+#include "xmodem/xmodem_transmitter.h"
+#include "serialport/libserialport.h"
+
+int main (int arc, char **argv )
+{
+   
+   return 0;
+}
