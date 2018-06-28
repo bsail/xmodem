bd7f5a80a30c633e7893a5ddc9068e1858cb046f
Nick Nerovny <nikolay.nerovniy@gmail.com>
2018-06-28 15:19:01 +0300

    Autoformatting with indent -linux -i2 -ts0

--- source/xmodem.c
+++ source/xmodem.c
@@ -8,65 +8,57 @@
 #include "xmodem.h"
 #include <config.h>
 
-uint8_t xmodem_calculate_crc(const uint8_t *data, const uint32_t size, uint16_t *result)
+uint8_t xmodem_calculate_crc(const uint8_t * data, const uint32_t size,
+                             uint16_t * result)
 {
 
-   uint16_t crc    = 0x0;
-   uint32_t count  = size;
-   uint8_t     status = false;
-   uint8_t  i      = 0;
+  uint16_t crc = 0x0;
+  uint32_t count = size;
+  uint8_t status = false;
+  uint8_t i = 0;
 
-   if (0 != data && 0 != result)
-   {
-           status = true;
+  if (0 != data && 0 != result) {
+    status = true;
 
-	   while (0 < count--)
-	   {
-	      crc = crc ^ (uint16_t) *data << 8;
-              data++;
-	      i = 8;
+    while (0 < count--) {
+      crc = crc ^ (uint16_t) * data << 8;
+      data++;
+      i = 8;
 
-	      do
-	      {
-		  if (0x8000 & crc)
-		  {
-		     crc = crc << 1 ^ 0x1021;
-		  }
-		  else
-		  {
-		     crc = crc << 1;
-		  }
+      do {
+        if (0x8000 & crc) {
+          crc = crc << 1 ^ 0x1021;
+        } else {
+          crc = crc << 1;
+        }
 
-	      } 
-	      while (0 < --i);
+      }
+      while (0 < --i);
 
-	   }
-           
-           *result = crc;
-   }
-
-   return status;
-}
-
-uint8_t xmodem_verify_packet(const xmodem_packet_t packet, uint8_t expected_packet_id)
-{
-    uint8_t     status         = false;
-    uint8_t     crc_status     = false;
-    uint16_t calculated_crc = 0;
-
-    crc_status = xmodem_calculate_crc(packet.data, XMODEM_BLOCK_SIZE, &calculated_crc);
-
-    if (packet.preamble == SOH &&
-        packet.id == expected_packet_id &&
-        packet.id_complement == 0xFF - packet.id &&
-        crc_status &&
-        calculated_crc == packet.crc)
-    {
-       status = true;
     }
 
-    return status;
+    *result = crc;
+  }
+
+  return status;
 }
 
+uint8_t xmodem_verify_packet(const xmodem_packet_t packet,
+                             uint8_t expected_packet_id)
+{
+  uint8_t status = false;
+  uint8_t crc_status = false;
+  uint16_t calculated_crc = 0;
 
+  crc_status =
+      xmodem_calculate_crc(packet.data, XMODEM_BLOCK_SIZE, &calculated_crc);
 
+  if (packet.preamble == SOH &&
+      packet.id == expected_packet_id &&
+      packet.id_complement == 0xFF - packet.id &&
+      crc_status && calculated_crc == packet.crc) {
+    status = true;
+  }
+
+  return status;
+}
--- source/xmodem_transmitter.h
+++ source/xmodem_transmitter.h
@@ -4,26 +4,39 @@
 
 #include <inttypes.h>
 
-enum XMODEM_TRANSMIT_STATES {XMODEM_TRANSMIT_INITIAL,               XMODEM_TRANSMIT_WAIT_FOR_C, 
-                             XMODEM_TRANSMIT_WAIT_FOR_C_ACK,        XMODEM_TRANSMIT_WRITE_BLOCK_FAILED,
-                             XMODEM_TRANSMIT_ABORT_TRANSFER,        XMODEM_TRANSMIT_WRITE_BLOCK, 
-                             XMODEM_TRANSMIT_C_ACK_RECEIVED,        XMODEM_TRANSMIT_COMPLETE,
-                             XMODEM_TRANSMIT_WRITE_EOT,             XMODEM_TRANSMIT_WAIT_FOR_EOT_ACK,
-                             XMODEM_TRANSMIT_TIMEOUT_EOT,           XMODEM_TRANSMIT_WRITE_BLOCK_TIMEOUT,
-                             XMODEM_TRANSMIT_WRITE_ETB,             XMODEM_TRANSMIT_WAIT_FOR_ETB_ACK,
-                             XMODEM_TRANSMIT_TIMEOUT_ETB,
-                             XMODEM_TRANSMIT_WAIT_WRITE_BLOCK,      XMODEM_TRANSMIT_UNKNOWN };
- typedef enum XMODEM_TRANSMIT_STATES xmodem_transmit_state_t;
+enum XMODEM_TRANSMIT_STATES { XMODEM_TRANSMIT_INITIAL,
+      XMODEM_TRANSMIT_WAIT_FOR_C,
+  XMODEM_TRANSMIT_WAIT_FOR_C_ACK, XMODEM_TRANSMIT_WRITE_BLOCK_FAILED,
+  XMODEM_TRANSMIT_ABORT_TRANSFER, XMODEM_TRANSMIT_WRITE_BLOCK,
+  XMODEM_TRANSMIT_C_ACK_RECEIVED, XMODEM_TRANSMIT_COMPLETE,
+  XMODEM_TRANSMIT_WRITE_EOT, XMODEM_TRANSMIT_WAIT_FOR_EOT_ACK,
+  XMODEM_TRANSMIT_TIMEOUT_EOT, XMODEM_TRANSMIT_WRITE_BLOCK_TIMEOUT,
+  XMODEM_TRANSMIT_WRITE_ETB, XMODEM_TRANSMIT_WAIT_FOR_ETB_ACK,
+  XMODEM_TRANSMIT_TIMEOUT_ETB,
+  XMODEM_TRANSMIT_WAIT_WRITE_BLOCK, XMODEM_TRANSMIT_UNKNOWN
+};
+typedef enum XMODEM_TRANSMIT_STATES xmodem_transmit_state_t;
 
 xmodem_transmit_state_t xmodem_transmit_state();
 
-uint8_t xmodem_transmit_init(uint8_t *buffer, uint32_t size);
+uint8_t xmodem_transmit_init(uint32_t size);
 uint8_t xmodem_transmit_process(const uint32_t current_time);
 uint8_t xmodem_transmitter_cleanup();
-void xmodem_transmitter_set_callback_write(uint8_t (*callback)(const uint32_t requested_size, uint8_t *buffer, uint8_t *write_status));
-void xmodem_transmitter_set_callback_read(uint8_t (*callback)(const uint32_t requested_size, uint8_t *buffer, uint32_t *returned_size));
-void xmodem_transmitter_set_callback_is_outbound_full(uint8_t (*callback)());
-void xmodem_transmitter_set_callback_is_inbound_empty(uint8_t (*callback)());
-void xmodem_transmitter_set_callback_get_buffer(uint8_t (*callback)(const uint32_t position, uint8_t *buffer));
+void
+xmodem_transmitter_set_callback_write(uint8_t(*callback)
+                                      (const uint32_t requested_size,
+                                       uint8_t * buffer,
+                                       uint8_t * write_status));
+void
+xmodem_transmitter_set_callback_read(uint8_t(*callback)
+                                     (const uint32_t requested_size,
+                                      uint8_t * buffer,
+                                      uint32_t * returned_size));
+void xmodem_transmitter_set_callback_is_outbound_full(uint8_t(*callback) ());
+void xmodem_transmitter_set_callback_is_inbound_empty(uint8_t(*callback) ());
+void
+xmodem_transmitter_set_callback_get_buffer(uint8_t(*callback)
+                                           (const uint32_t position,
+                                            uint8_t * buffer));
 
 #endif
